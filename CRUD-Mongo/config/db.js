d7ef7f2ee8370408f9802ecaf90141e79beac249
Jesus Mares <jesusalberto.0330@itsjc.edu.mx>
2026-01-31 11:50:33 -0600

    configuracion de la coneccion a db

--- CRUD-Mongo/config/db.js
+++ CRUD-Mongo/config/db.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+const conectardb = async() => {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/crud_node');
+        console.log('coneccion exitosa');
+    } catch (error) {
+        console.log('Tas mal chato');
+        process.exit(1);
+    }
+};
+module.exports = conectardb;
